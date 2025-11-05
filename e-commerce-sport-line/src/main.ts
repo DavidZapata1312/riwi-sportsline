9f7894dc6b26a8c4ffbe61bbab792d86775d3b18
AndresRestrepoDev <restreporamirezandresfelipe@gmail.com>
2025-11-05 14:04:25 -0500

    feat: HU-1 terminada

--- e-commerce-sport-line/src/main.ts
+++ e-commerce-sport-line/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3002;
+
+  await app.listen(port);
+
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Documentación disponible en http://localhost:${port}/api`);
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error('Error al iniciar la aplicación:', err);
+  process.exit(1);
+});
